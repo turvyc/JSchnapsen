12f7cc10b0bf25141b1f85d8d5ba2c04c0676a0f
T. Colin Strong <turvyc@gmail.com>
2015-12-17 14:45:59 -0800

    Created basic GUI.

--- GameFrame.java
+++ GameFrame.java
@@ -1,23 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+/**
+ * Creates the main frame of the game, including the menu bar.
+ */
 public class GameFrame extends JFrame {
 
     private GameController controller;
+    private MenuItemListener listener;
 
-    private final int FRAME_WIDTH = 500;
-    private final int FRAME_HEIGHT = 500;
+    private final int FRAME_WIDTH = 1000;
+    private final int FRAME_HEIGHT = 1000;
     private String FRAME_TITLE = "JSchnapsen";
+
+    private String MENU_ITEM_EXIT = "Exit";
+    private String MENU_ITEM_BLANK = "Blank";
+    private String MENU_ITEM_ABOUT = "About";
+
+    class MenuItemListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getActionCommand().equals(MENU_ITEM_EXIT))
+                System.exit(0);
+        }
+    }
 
     public GameFrame(GameController c) {
         controller = c;
+        listener = new MenuItemListener();
 
-        // Set window properties
+        // Set frame properties
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
         setTitle(FRAME_TITLE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
 
         // Create the menu
         JMenuBar menuBar = new JMenuBar();
@@ -25,19 +54,71 @@ public class GameFrame extends JFrame {
         menuBar.add(createFileMenu());
         menuBar.add(createOptionsMenu());
         menuBar.add(createAboutMenu());
+
+        // Create the main panels
+        JPanel opponentPanel = createOpponentPanel();
+        JPanel centerPanel = createCenterPanel();
+        JPanel playerPanel = createPlayerPanel();
+        JPanel buttonPanel = createButtonPanel();
+
+        // Add the panels to the frame, according to the constraints
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = 0.5;
+        constraints.weighty = 0.5;
+        add(opponentPanel, constraints);
+
+        constraints.gridy = 1;
+        constraints.weighty = 1;
+        add(centerPanel, constraints);
+
+        constraints.gridy = 2;
+        constraints.weighty = 0.5;
+        add(playerPanel, constraints);
+
+        constraints.gridy = 3;
+        constraints.weighty = 0.1;
+        add(buttonPanel, constraints);
+
+    }
+
+    private JPanel createOpponentPanel() {
+        JPanel p = new JPanel();
+        p.setBackground(Color.RED);
+        return p;
+    }
+
+    private JPanel createCenterPanel() {
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.setBackground(Color.GREEN);
+        return p;
+    }
+
+    private JPanel createPlayerPanel() {
+        JPanel p = new JPanel();
+        p.setBackground(Color.BLUE);
+        return p;
+    }
+
+    private JPanel createButtonPanel() {
+        JPanel p = new JPanel();
+        p.setBackground(Color.YELLOW);
+        return p;
     }
 
     private JMenu createFileMenu() {
         JMenu menu = new JMenu("File");
 
         // Define the items
-        JMenuItem exit = new JMenuItem("Exit");
+        JMenuItem exit = new JMenuItem(MENU_ITEM_EXIT);
 
         // Add them to the menu
         menu.add(exit);
 
         // Attach them to the controller
-        exit.addActionListener(controller);
+        exit.addActionListener(listener);
 
         return menu;
     }
@@ -46,13 +127,13 @@ public class GameFrame extends JFrame {
         JMenu menu = new JMenu("Options");
 
         // Define the items
-        JMenuItem blank = new JMenuItem("Blank");
+        JMenuItem blank = new JMenuItem(MENU_ITEM_BLANK);
 
         // Add them to the menu
         menu.add(blank);
 
         // Attach them to the controller
-        blank.addActionListener(controller);
+        blank.addActionListener(listener);
 
         return menu;
     }
@@ -61,13 +142,13 @@ public class GameFrame extends JFrame {
         JMenu menu = new JMenu("About");
 
         // Define the items
-        JMenuItem about = new JMenuItem("About JSchnapsen");
+        JMenuItem about = new JMenuItem(MENU_ITEM_ABOUT);
 
         // Add them to the menu
         menu.add(about);
 
         // Attach them to the controller
-        about.addActionListener(controller);
+        about.addActionListener(listener);
 
         return menu;
     }
