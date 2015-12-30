12fe85f10eebe501771465a15fe772b1bfcb27c0
T. Colin Strong <turvyc@gmail.com>
2015-12-30 14:20:06 -0800

    Deleted setVisible from Hand, because it was already implemented in the parent class.

--- Hand.java
+++ Hand.java
@@ -3,15 +3,6 @@
  */
 public class Hand extends CardGrouping {
 
-    /**
-     * Sets the entire hand to be visible or not.
-     * @param b the hand is visible to the user if true, covered if false
-     */
-    public void setVisible(boolean b) {
-        for (Card c : cards)
-            c.setVisible(b);
-    }
-
     @Override
     public String toString() {
         String s = "Hand: ";
